8b8a2ccb714088f0b70de4418748617d8c071584
nubang <nubang7@gmail.com>
2019-03-19 23:52:55 +0900

    FibonacciRecursiveTest 테스트 케이스 클래스 이름 변경

--- src/test/java/fibonacci/recursive/FibonacciRecursivceTest.java
+++ src/test/java/fibonacci/recursive/FibonacciRecursivceTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class FibonacciRecursivce {
+public class FibonacciRecursivceTest {
 
 	private FibonacciRecursive fibonacci;
 	
